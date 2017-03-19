27314898eac90ef903a2510f6b4a25c1b9ab1682
larenSakota <jedi.temple@yahoo.com>
2017-03-19 07:33:34 -0700

    completed image selection and create new contact

--- Contacts Redo/ContactsTableViewController.swift
+++ Contacts Redo/ContactsTableViewController.swift
@@ -17,16 +17,19 @@ class ContactsTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         navigationItem.leftBarButtonItem = editButtonItem
         
-        loadSampleContacts()
-        
+        // Load saved contacts, otherwise load sample contacts
+        if let savedContacts = loadContacts() {
+            contacts += savedContacts
+        } else {
+            loadSampleContacts()
+        }
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
@@ -60,32 +63,46 @@ class ContactsTableViewController: UITableViewController {
     }
 
 
-    /*
+
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
-
+    
 
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            contacts.remove(at: indexPath.row)
+            saveContacts()
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        if tableView.isEditing {
+            return .none
+        } else {
+            return .delete
+        }
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let contactMoving = contacts.remove(at: fromIndexPath.row)
+        contacts.insert(contactMoving, at: destinationIndexPath.row)
+        tableView.reloadData()
+        
+    }
 
     /*
     // Override to support conditional rearranging of the table view.
@@ -118,12 +135,29 @@ class ContactsTableViewController: UITableViewController {
             fatalError("Unexpected Segue Identifier; \(segue.identifier)")
         }
     }
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        let indexPath = self.tableView.indexPath(for: sender as! UITableViewCell)!
-//        let contact = self.contacts[indexPath.row]
-//        let destination = segue.destination as! DetailViewController
-//        destination.contact = contact
-//    }
+
+    //MARK: Actions
+    
+    @IBAction func unwindToContactList(sender: UIStoryboardSegue) {
+        if let sourceViewController = sender.source as? DetailViewController, let contact = sourceViewController.contact {
+            
+            if let selectedIndexPath = tableView.indexPathForSelectedRow {
+                // Update an existing contact.
+                contacts[selectedIndexPath.row] = contact
+                tableView.reloadRows(at: [selectedIndexPath], with: .none)
+            }
+            else {
+                // Add a new contact.
+                let newIndexPath = IndexPath(row: contacts.count, section: 0)
+                
+                contacts.append(contact)
+                tableView.insertRows(at: [newIndexPath], with: .automatic)
+            }
+            
+            // Save the contacts.
+            saveContacts()
+        }
+    }
     
     //MARK: Private Methods
     
--- Contacts Redo/DetailViewController.swift
+++ Contacts Redo/DetailViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import os.log
 
 
-class DetailViewController: UIViewController, UITextFieldDelegate, UINavigationControllerDelegate {
+class DetailViewController: UIViewController, UITextFieldDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     @IBOutlet weak var photo: UIImageView!
     @IBOutlet weak var nameText: UITextField!
@@ -25,20 +25,69 @@ class DetailViewController: UIViewController, UITextFieldDelegate, UINavigationC
         // Set image to round
         photo.layer.cornerRadius = photo.frame.size.width/2
         photo.clipsToBounds = true
+        photo.layer.borderWidth = 1
         
-        // Populate label fields
+        nameText.delegate = self
+        
+        // Populate Contact fields
         if let contact = self.contact {
+            navigationItem.title = contact.name
             self.nameText.text = contact.name
             self.phoneText.text = contact.number
             self.photo.image = contact.photo
+        
         }
         
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Done button state
+        updateDoneButtonState()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: UITextFieldDelegate
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        // Disable the Done button while editing.
+        doneBtn.isEnabled = false
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        updateDoneButtonState()
+        navigationItem.title = nameText.text
+    }
+    
+    
+    //MARK: UIImagePickerControllerDelegate
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        // Set photo to display the selected image.
+        photo.image = selectedImage
+        photo.layer.cornerRadius = photo.frame.size.width/2
+        photo.clipsToBounds = true
+        
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
     }
     
     // MARK: Navigation
@@ -56,6 +105,43 @@ class DetailViewController: UIViewController, UITextFieldDelegate, UINavigationC
         else {
             fatalError("The DetailViewController is not inside a navigation controller.")
         }
+    }
+    
+    // This method lets you configure a view controller before it's presented.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        super.prepare(for: segue, sender: sender)
+        
+        // Configure the destination view controller only when the done button is pressed.
+        guard let button = sender as? UIBarButtonItem, button === doneBtn else {
+            os_log("The done button was not pressed, cancelling", log: OSLog.default, type: .debug)
+            return
+        }
+        
+        let name = nameText.text ?? ""
+        let number = phoneText.text ?? ""
+        let pic = photo.image
+        
+        // Set the contact to be passed to ContactTableViewController after the unwind segue.
+        contact = Contact(name: name, number: number, photo: pic)
+    }
+    
+    //MARK: Actions
+    
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        
+        // Hide the keyboard.
+        nameText.resignFirstResponder()
+        
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        let imagePickerController = UIImagePickerController()
+        
+        // Only allow photos to be picked, not taken.
+        imagePickerController.sourceType = .photoLibrary
+        
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
     }
     
     //MARK: Private Methods
